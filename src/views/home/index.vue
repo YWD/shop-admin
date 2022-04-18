d324e04948f254fc24df719be244362c34250880
杨文冬 <1347011912@qq.com>
2022-04-18 12:15:43 +0800

    build: config touter and vuex

---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>首页</h1>
+  <div>{{ counter }}</div>
+</template>
+
+<script setup lang="ts">
+import { useMainStore } from '../../store/main'
+import { storeToRefs } from 'pinia'
+const main = useMainStore()
+const { counter } = storeToRefs(main)
+</script>
+
+<style scoped>
+
+</style>
