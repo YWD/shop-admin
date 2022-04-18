6c354f0c62cfa9187edc11493f8d37fd1a955ba0
杨文冬 <1347011912@qq.com>
2022-04-18 22:02:51 +0800

    build: element-plus add

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import './styles/index.scss'
+import elementPlus from '@/plugins/element-plus'
 
 createApp(App)
   .use(router)
   .use(createPinia())
+  .use(elementPlus)
   .mount('#app')
